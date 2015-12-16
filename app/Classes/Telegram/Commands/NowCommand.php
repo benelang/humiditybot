fd3b8490b90a4f85b4ec26857062c6ed0d738b17
Benedict Lang <benedict.lang@commumee.de>
2015-12-16 14:20:56 +0100

    error handling in now command

--- app/Classes/Telegram/Commands/NowCommand.php
+++ app/Classes/Telegram/Commands/NowCommand.php
@@ -29,34 +29,47 @@ class NowCommand extends Command
         $update = $this->getUpdate();
         $from = $update->getMessage()->getFrom()->getId();
         $user = User::where('chat_id', '=', $from)->first();
-        $devices = $user->devices;
+        echo $user;
+        if (!$user) {
+          $response = "Hi, " . $update->getMessage()->getFrom()->getFirstName() . "!" . PHP_EOL . "Ich würde dir gerne helfen, aber leider kennen wir uns noch gar nicht. Damit wir uns kennen lernen können, besuche bitte humiditybot.com (noch nicht verfügbar).";
+        } else {
+          $devices = $user->devices;
+          if (count($devices) == 0) {
+            $response = "Hi, " . $update->getMessage()->getFrom()->getFirstName() . "!" . PHP_EOL . "Ich würde dir gerne helfen, aber du hast leider keine Sensoren registriert. Ich kann dir einen Particle Photon und einen DHT-22 Sensor empfehlen.";
+          } else {
+            $response = "Aktuelle Messwerte:" . PHP_EOL;
+            foreach ($devices as $device) {
+              $values = Values::where('device_id', '=', $device->id)->orderBy('id', 'DESC')->take(2)->get();
+              if (!$values) {
+                $response = "Es tut mir furchtbar leid, aber ich kann in meinem Durcheinander leider keine Werte für dich finden.";
+              } else {
+                if ($values[0]->humidity > $values[1]->humidty) {
+                  $trendHumidityCode = "\xE2\xAC\x86"; //going up
+                } if ($values[0]->humidity == $values[1]->humidity){
+                  $trendHumidityCode = "\xE2\x9E\xA1"; //staying the same
+                } if ($values[0]->humidity < $values[1]->humidity){
+                  $trendHumidityCode = "\xE2\xAC\x87"; //going down
+                }
 
-        $response = "Aktuelle Messwerte:" . PHP_EOL;
-        foreach ($devices as $device) {
-          $values = Values::where('device_id', '=', $device->id)->orderBy('id', 'DESC')->take(2)->get();
-          if ($values[0]->humidity > $values[1]->humidty) {
-            $trendHumidityCode = "\xE2\xAC\x86"; //going up
-          } if ($values[0]->humidity == $values[1]->humidity){
-            $trendHumidityCode = "\xE2\x9E\xA1"; //staying the same
-          } if ($values[0]->humidity < $values[1]->humidity){
-            $trendHumidityCode = "\xE2\xAC\x87"; //going down
+                if ($values[0]->temparature > $values[1]->temparature) {
+                  $trendTemparatureCode = "\xE2\xAC\x86"; //going up
+                } if ($values[0]->temparature == $values[1]->temparature){
+                  $trendTemparatureCode = "\xE2\x9E\xA1"; //staying the same
+                } if ($values[0]->temparature < $values[1]->temparature){
+                  $trendTemparatureCode = "\xE2\xAC\x87"; //going down
+                }
+
+                $response .= "Für " . $device->description . " wurden folgende Messwerte gefunden:" . PHP_EOL;
+                $response .= "Luftfeuchtigkeit: " . $values[0]->humidity . "% " . $trendHumidityCode . PHP_EOL;
+                $response .= "Temparatur: " . $values[0]->temparature . "°C " . $trendTemparatureCode . PHP_EOL;
+                date_default_timezone_set('Europe/Berlin');
+                $time = strtotime($values[0]->time. ' UTC');
+                $response .= "Gemessen am " . date('d.m.Y', $time) . ", um " . date('H:i:s', $time) . PHP_EOL;
+              }
+            }
           }
-
-          if ($values[0]->temparature > $values[1]->temparature) {
-            $trendTemparatureCode = "\xE2\xAC\x86"; //going up
-          } if ($values[0]->temparature == $values[1]->temparature){
-            $trendTemparatureCode = "\xE2\x9E\xA1"; //staying the same
-          } if ($values[0]->temparature < $values[1]->temparature){
-            $trendTemparatureCode = "\xE2\xAC\x87"; //going down
-          }
-
-          $response .= "Für " . $device->description . " wurden folgende Messwerte gefunden:" . PHP_EOL;
-          $response .= "Luftfeuchtigkeit: " . $values[0]->humidity . "% " . $trendHumidityCode . PHP_EOL;
-          $response .= "Temparatur: " . $values[0]->temparature . "°C " . $trendTemparatureCode . PHP_EOL;
-          date_default_timezone_set('Europe/Berlin');
-          $time = strtotime($values[0]->time. ' UTC');
-          $response .= "Gemessen am " . date('d.m.Y', $time) . ", um " . date('H:i:s', $time) . PHP_EOL;
         }
+
 
         $this->replyWithMessage($response);
     }
